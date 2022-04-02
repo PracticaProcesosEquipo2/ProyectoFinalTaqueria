05f11b42a245f80568a463982ad6b87cceba43b8
RamiroHernandezB <99367449+RamiroHernandezB@users.noreply.github.com>
2022-04-01 18:11:13 -0600

    Primer commit de Ramiro de Jesús Hernández Bernabé

---
+++ Taqueria/src/taqueria/Taqueria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taqueria;
+
+/**
+ *
+ * @author ramirohb
+ */
+public class Taqueria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
